913a1c9f1f11c8fa674be2fabaa4be37ca4533ed
Sergei Komarov <serg.komarov@gmail.com>
2023-11-10 00:21:20 +0300

    create react project

---
+++ my-app/src/App.js
@@ -0,0 +1,27 @@
+//  начинается императивный стиль
+import logo from "./logo.svg";
+import "./App.css";
+
+export const App = () => {
+	let currentYear = new Date().getFullYear();
+	return (
+		//  начинается декларативный стиль
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				<p>
+					Edit <code>src/App.js</code> and save to reload. Very cool!
+				</p>
+				<a
+					className="App-link"
+					href="https://reactjs.org"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					Learn React
+				</a>
+				<p> {currentYear} </p>
+			</header>
+		</div>
+	);
+};
