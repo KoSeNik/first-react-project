5de31256faaa900f30fbbcbaa85a36200fc6b203
Sergei Komarov <serg.komarov@gmail.com>
2023-11-10 01:22:36 +0300

    Rewrote the App.js component without using JSX

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,27 +1,46 @@
 //  начинается императивный стиль
 import logo from "./logo.svg";
 import "./App.css";
+import { createElement } from "react";
 
 export const App = () => {
 	let currentYear = new Date().getFullYear();
 	return (
 		//  начинается декларативный стиль
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload. Very cool!
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-				<p> {currentYear} </p>
-			</header>
-		</div>
+		createElement(
+			"div",
+			{
+				className: "App",
+			},
+			createElement(
+				"header",
+				{
+					className: "App-header",
+				},
+				createElement("img", {
+					src: logo,
+					className: "App-logo",
+					alt: "logo",
+				}),
+				createElement(
+					"p",
+					null,
+					"Edit ",
+					createElement("code", null, "src/App.js"),
+					" and save to reload. Very cool!",
+				),
+				createElement(
+					"a",
+					{
+						className: "App-link",
+						href: "https://reactjs.org",
+						target: "_blank",
+						rel: "noopener noreferrer",
+					},
+					"Learn React",
+				),
+				createElement("p", null, " ", currentYear, " "),
+			),
+		)
 	);
 };
